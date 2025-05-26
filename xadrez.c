140ffe3c0d1e6f29584297baf1aa1c5f86904e88
Hotline <155464188+Hotlinew@users.noreply.github.com>
2025-05-26 18:24:29 +0000

    Xadrez nivel básico

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,68 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
+    int mov_Bispo = 5, mov_Torre = 5, mov_Rainha = 8;
+    int menu;
+
+    do {
+
+    printf("\nmover:\n"); 
+
+     printf("    ");
+    if (mov_Bispo == 5) printf ("  1. Bispo");
+    if (mov_Torre == 5) printf ("  2. Torre");
+    if (mov_Rainha == 8) printf ("  3. Rainha");
+    printf("  4. Sair");
+     printf("\n");
+
+    scanf("%d", &menu);
+
+    switch (menu) {
+    case 1://Bispo
+     if (mov_Bispo == 5){
+
+      for (int i = 0; i <= mov_Bispo; i++) {
+          printf("Cima - "); printf("Direita\n"); }
+          mov_Bispo++; 
+
+    } else {
+        printf("erro, numero invalido");
+    }
+    break;
+
+    case 2://Torre
+     if (mov_Torre == 5){
+      for (int i = 0; i <= mov_Torre; i++) {
+         printf("Direita\n"); }
+          mov_Torre++;
+    } else {
+        printf("erro, numero invalido");
+    }
+    break;
+
+    case 3://Rainha
+     if (mov_Rainha == 8) {
+      for (int i = 0; i <= mov_Rainha; i++) {
+          printf("Esquerda\n"); }
+          mov_Rainha++;
+    } else {
+        printf("erro, numero invalido");
+    }
+    break;
+
+    case 4://sair
+
+    break;
+    
+    default:
+    printf("erro, numero invalido");
+    break;
+    }
+
+    } while (menu != 4);
+    
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
